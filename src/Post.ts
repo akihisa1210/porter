8578292c458f3cac4dce299da38fcbaa3232fc01
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2020-05-23 02:57:11 +0900

    add a test for post content

--- src/Post.ts
+++ src/Post.ts
@@ -18,19 +18,20 @@ export class Post {
     return `[${amazonBibliograhicInformation.imageUrl} ${
       amazonBibliograhicInformation.currentUrl
     }]
-  ${this.makeAuthorsLink(amazonBibliograhicInformation.authors).join(" ")}
-  ${amazonBibliograhicInformation.publishInfo.publisher} ${
+${this.makeAuthorsLink(amazonBibliograhicInformation.authors).join(" ")}
+${amazonBibliograhicInformation.publishInfo.publisher} ${
       amazonBibliograhicInformation.publishInfo.publishDate
     }
-  ISBN/ASIN: ${amazonBibliograhicInformation.asin}
-  > ${amazonBibliograhicInformation.description.replace(/\n/g, "\n>")}
-  #本`;
+ISBN/ASIN: ${amazonBibliograhicInformation.asin}
+>${amazonBibliograhicInformation.description.replace(/\n/g, "\n>")}
+#本
+`;
   }
 
   private makeAuthorsLink = (authors: AuthorInfo[]): string[] => {
     const authorsLink: string[] = [];
     for (const author of authors) {
-      authorsLink.push(`[ ${author.author} ]${author.contribution}`);
+      authorsLink.push(`[${author.author}]${author.contribution}`);
     }
     return authorsLink;
   };
---
+++ test/Posts.test.ts
@@ -0,0 +1,40 @@
+import { Post } from "../src/Post";
+import { PaperBookAmazonBibliograhicInformation } from "../src/PaperBookAmazonBibliograhicInformation";
+jest.mock("../src/PaperBookAmazonBibliograhicInformation");
+
+test("constructScrapboxPageContent() constructs valid bibliographic information", () => {
+  const post = new Post();
+  const sampleBibliographicInformation = new PaperBookAmazonBibliograhicInformation();
+  sampleBibliographicInformation.productTitle = "sampleProductTitel";
+  sampleBibliographicInformation.asin = "sampleAsin";
+  sampleBibliographicInformation.publishInfo = {
+    publisher: "samplePublisher",
+    publishDate: "samplePublishDate",
+  };
+  sampleBibliographicInformation.description = `sampleDescription1
+sampleDescription2
+sampleDescription3`;
+  sampleBibliographicInformation.imageUrl = "sampleImageUrl";
+  sampleBibliographicInformation.authors = [
+    {
+      author: "author1",
+      contribution: "(contribution1)",
+    },
+    {
+      author: "author2",
+      contribution: "(contribution2)",
+    },
+  ];
+  sampleBibliographicInformation.currentUrl = "sampleCurrentUrl";
+
+  expect(post.constructScrapboxPageContent(sampleBibliographicInformation))
+    .toBe(`[sampleImageUrl sampleCurrentUrl]
+[author1](contribution1) [author2](contribution2)
+samplePublisher samplePublishDate
+ISBN/ASIN: sampleAsin
+>sampleDescription1
+>sampleDescription2
+>sampleDescription3
+#本
+`);
+});

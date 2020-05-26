743c41a97e510feb77c7cc5b8ff657fd5a25f89d
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2020-05-27 02:35:57 +0900

    Add peper book judge test

--- test/site/amazon.test.ts
+++ test/site/amazon.test.ts
@@ -1,6 +1,10 @@
 import { Amazon } from "../../src/site/amazon";
 
 test("Paper book is selected", () => {
+  const dom = `<li class="swatchElement"></li>
+<li class="swatchElement selected"></li>`;
+  document.body.innerHTML = dom;
+
   const amazon = new Amazon();
-  expect(amazon.isEbook).toBe(false);
+  expect(amazon.isEbook()).toBe(false);
 });

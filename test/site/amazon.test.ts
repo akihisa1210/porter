f4ff031657747743eb62736beaae2af0a705d6a4
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2020-05-27 20:45:54 +0900

    Add book kind judge test

--- test/site/amazon.test.ts
+++ test/site/amazon.test.ts
@@ -1,7 +1,16 @@
 import { Amazon } from "../../src/site/amazon";
 
+test("Ebook is selected", () => {
+  const dom = `<li class="swatchElement selected"><span>Kindle版 (電子書籍)</span></li>
+<li class="swatchElement"></li>`;
+  document.body.innerHTML = dom;
+
+  const amazon = new Amazon();
+  expect(amazon.isEbook()).toBe(true);
+});
+
 test("Paper book is selected", () => {
-  const dom = `<li class="swatchElement"></li>
+  const dom = `<li class="swatchElement"><span>Kindle版 (電子書籍)</span></li>
 <li class="swatchElement selected"></li>`;
   document.body.innerHTML = dom;
 

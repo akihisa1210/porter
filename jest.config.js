37c4c76cddfa5eefe057b38fcf707bbdc43fc8d9
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-02-23 01:31:02 +0900

    fix: 🐛 reflect line feed in description
    ✅ Closes: #51

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  testEnvironmentOptions: { resources: "usable" },
   collectCoverage: true,
   collectCoverageFrom: ["./src/**/*.ts"],
 };

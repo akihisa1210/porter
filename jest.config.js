e2fd201fb02feb50b3c9e3c67d67d0eb9537363f
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2022-03-05 02:44:34 +0900

    test: output lcov.info

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  testEnvironmentOptions: { resources: "usable" },
-  collectCoverage: true,
-  collectCoverageFrom: ["./src/**/*.ts"],
-};

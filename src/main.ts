e57a6eb777819a370a560585b8bc1dbfbdedd7b8
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2022-03-06 18:09:33 +0900

    refactor: use new classes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,14 @@
-import { PostTitle } from "./Post";
-import { Amazon } from "./site/amazon";
-import { BibInfoFactory } from "./bibInfo/bibInfoFactory";
-import { Bibliography } from "./bibliography/bibliography";
 import { ScrapboxPoster } from "./poster/scrapboxPoster";
+import { AmazonScraper } from "./scraper/amazonScraper";
 
 const main = (): void => {
-  const amazon = new Amazon();
-  let bibInfo;
-  const factory = new BibInfoFactory();
-  if (amazon.isEbook()) {
-    console.log("This is ebook.");
-    bibInfo = factory.createBibInfo("AmazonEbookBibInfo");
-    console.log("AmazonEbookBibInfo", bibInfo);
-  } else {
-    console.log("This is paper book.");
-    bibInfo = factory.createBibInfo("AmazonPaperBookBibInfo");
-    console.log("PaperBookAmazonBibliograhicInformation", bibInfo);
-  }
+  const scraper = new AmazonScraper();
+  const bibliography = scraper.run();
 
-  new PostTitle(
-    window.prompt('Scrap "Amazon" to your scrapbox.', `『${bibInfo.title}』`)
+  window.prompt(
+    'Scrap "Amazon" to your scrapbox.',
+    `『${bibliography.title}』`
   );
-
-  // TODO: remove BibInfo
-  const authors = bibInfo.authorsInfo.map((authorInfo) => {
-    return {
-      name: authorInfo.author,
-      contribution: authorInfo.contribution.replace(/\(|\)|,/g, ""),
-    };
-  });
-  const bibliography: Bibliography = {
-    title: bibInfo.title,
-    imageUrl: bibInfo.imageUrl,
-    sourceUrl: bibInfo.sourceUrl,
-    authors: authors,
-    publisher: bibInfo.publishInfo.publisher,
-    publicationDate: bibInfo.publishInfo.publishDate,
-    isbn: bibInfo.isbn,
-    description: bibInfo.description,
-  };
 
   const poster = new ScrapboxPoster(bibliography);
   poster.run();
